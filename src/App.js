9ea48e67faf5ca61ce047500acbc77d9a718e2ed
MikkoJaakola <mikko.jaakola@hotmail.fi>
2020-04-03 11:30:23 +0300

    Added AppBar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import React from 'react';
 import Customerlist from './components/Customerlist';
 import Traininglist from './components/Traininglist';
 
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+
+
+
 import './App.css';
 
 function App() {
+
+
+  
+  
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" >
+            Customers
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+
         <Customerlist />
+
+        <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" >
+            Trainings
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      
         <Traininglist />
+      
+      
+        
     </div>
   );
 }
